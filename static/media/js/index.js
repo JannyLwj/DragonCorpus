62b25d01b8163ec11e9e1be856cc39b61cfe7802
Janny <Wenjuan.li@nuance.com>
2018-02-01 10:35:57 +0800

    20180201_v2

--- static/media/js/index.js
+++ static/media/js/index.js
@@ -715,47 +715,6 @@ var Index = function () {
 
             setTimeout(function () {
 
-                $('#styler').pulsate({
-                    color: "#bb3319",
-                    repeat: 10
-                });
-
-                $.extend($.gritter.options, {
-                    position: 'top-left'
-                });
-
-                var unique_id = $.gritter.add({
-                    position: 'top-left',
-                    // (string | mandatory) the heading of the notification
-                    title: 'Customize Metronic!',
-                    // (string | mandatory) the text inside the notification
-                    text: 'Metronic allows you to easily customize the theme colors and layout settings.',
-                    // (string | optional) the image to display on the left
-                    image1: './assets/img/avatar1.png',
-                    // (bool | optional) if you want it to fade out on its own or just sit there
-                    sticky: true,
-                    // (int | optional) the time you want it to be alive for before fading out
-                    time: '',
-                    // (string | optional) the class name you want to apply to that specific message
-                    class_name: 'my-sticky-class'
-                });
-
-                $.extend($.gritter.options, {
-                    position: 'top-right'
-                });
-
-                // You can have it return a unique id, this can be used to manually remove it later using
-                setTimeout(function () {
-                    $.gritter.remove(unique_id, {
-                        fade: true,
-                        speed: 'slow'
-                    });
-                }, 15000);
-
-            }, 23000);
-
-            setTimeout(function () {
-
                 $.extend($.gritter.options, {
                     position: 'top-left'
                 });
